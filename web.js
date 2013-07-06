3041e129076544fb068113d58f5daaffc56f9a80
Ubuntu <ubuntu@ip-172-31-30-234.us-west-2.compute.internal>
2013-07-06 21:59:56 +0000

    zoops

--- web.js
+++ web.js
@@ -2,8 +2,18 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var getString(){
+  var buffer = new Buffer(16);
+  fs.readFile('index.html', function(err, data){
+	if(err) throw err;
+	buff.write(data);
+  };
+  return buffer;
+}
+
 app.get('/', function(request, response) {
-  response.send('Hello Katie!');
+  var buffer = getString();
+  response.send(buffer.toString() );
 });
 
 var port = process.env.PORT || 5000;
