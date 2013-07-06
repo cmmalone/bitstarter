8cbe98a1b933a46a09195b8311d5401fc7d16b02
Ubuntu <ubuntu@ip-172-31-30-234.us-west-2.compute.internal>
2013-07-06 23:00:34 +0000

    does it work?

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 function getString(){
-  var data = fs.readFile("index.html", "utf8");
+  var data = fs.readFileSync("index.html", "utf8");
   console.log(data);
   return data;  
 };
@@ -12,12 +12,14 @@ function getString(){
 app.get('/', function(request, response) {
 //  var buffer = getString();
 //  console.log(buffer );
-  var string = getString();
-  response.send(string );
-  console.log( string );
+  var str = getString();
+  response.send(str );
+  console.log( str );
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
